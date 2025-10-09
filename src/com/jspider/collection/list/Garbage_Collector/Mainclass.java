44aee5037c5e252f6dc235e627af5d6f7fb70e8f
prasanta-123 <prasantamallik195@gmail.com>
2025-10-10 00:07:51 +0530

    garbage collector

---
+++ src/com/jspider/collection/list/Garbage_Collector/Mainclass.java
@@ -0,0 +1,25 @@
+package com.jspider.collection.list.Garbage_Collector;
+
+public class Mainclass {
+    static class Sample1 extends Object
+    {
+        @Override
+        protected void finalize() throws Throwable{
+            System.out.println("finalize() executed..");
+        }
+
+    }
+
+    public static void main(String[] args) {
+       Sample1 s1= new Sample1();
+        System.out.println(s1.hashCode());
+
+        for (int i = 1; i <= 10; i++) {
+            System.out.println(i);
+        }
+        System.out.println("Program Ends...");
+    }
+
+
+
+    }
